4087560681d7e12a6940666b89c55090bba72257
oldrover <oldrover@web.de>
2021-01-19 14:59:31 +0100

    added testmethod getOrdersForUserNotFound

---
+++ src/test/java/com/example/demo/controllers/OrderControllerTest.java
@@ -0,0 +1,97 @@
+package com.example.demo.controllers;
+
+import com.example.demo.model.persistence.Cart;
+import com.example.demo.model.persistence.Item;
+import com.example.demo.model.persistence.User;
+import com.example.demo.model.persistence.UserOrder;
+import com.example.demo.model.persistence.repositories.OrderRepository;
+import com.example.demo.model.persistence.repositories.UserRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+
+public class OrderControllerTest {
+
+    @InjectMocks
+    OrderController orderController;
+
+    @Mock
+    UserRepository userRepository;
+
+    @Mock
+    OrderRepository orderRepository;
+
+    User user;
+    UserOrder userOrder;
+    Item item;
+
+    @Before
+    public void setUp() {
+
+        MockitoAnnotations.initMocks(this);
+
+        user = new User();
+        user.setUsername("testUser");
+
+        item = new Item();
+        item.setId(1L);
+        item.setName("testItem");
+
+        Cart cart = new Cart();
+        cart.setItems(Collections.singletonList(item));
+        user.setCart(cart);
+        cart.setUser(user);
+
+        userOrder = new UserOrder();
+        userOrder.setId(1L);
+        userOrder.setUser(user);
+
+        when(userRepository.findByUsername("testUser")).thenReturn(user);
+        when(orderRepository.findByUser(user)).thenReturn(Collections.singletonList(userOrder));
+
+    }
+
+    @Test
+    public void submitOrderSuccess() {
+        ResponseEntity<UserOrder> response = orderController.submit("testUser");
+        assertNotNull(response);
+        assertEquals(200, response.getStatusCodeValue());
+
+        UserOrder order = response.getBody();
+        assertNotNull(order);
+        assertEquals(user, order.getUser());
+        assertTrue(order.getItems().contains(item));
+    }
+
+    @Test
+    public void getOrdersForUserSuccess() {
+        ResponseEntity<List<UserOrder>> response = orderController.getOrdersForUser("testUser");
+        assertNotNull(response);
+        assertEquals(200, response.getStatusCodeValue());
+
+        List<UserOrder> orderList = response.getBody();
+        assertNotNull(orderList);
+        assertTrue(orderList.contains(userOrder));
+
+    }
+
+    @Test
+    public void getOrdersForUserNotFound() {
+        ResponseEntity<List<UserOrder>> response = orderController.getOrdersForUser("notfound");
+        assertNotNull(response);
+        assertEquals(404, response.getStatusCodeValue());
+
+    }
+
+
+
+}
